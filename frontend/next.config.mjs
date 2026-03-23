289cc294e4c024d06c8d88b6af07cd42960d6037
ZeroHardCore <allancarvalho01@gmail.com>
2026-03-22 21:08:48 -0300

    fix: alinha next config com AUTH_ARCHITECTURE

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: process.platform === 'win32' ? undefined : 'standalone',
+  output: 'standalone',
 };
 export default nextConfig;
